4ab82b3a5a8b1ee544af8b3b963913e384329155
Bayu Nugroho <nugrohob825@gmail.com>
2022-12-02 17:07:50 +0700

    feat: dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 // import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './pages/about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TableComponent } from './table/table.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     // HomepageComponent,
-    AboutComponent
+    AboutComponent,
+    routingComponent,
+    NavbarComponent,
+    SidebarComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
